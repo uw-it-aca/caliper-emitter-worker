ca5374e3ffb0646b9580f8449d67c7fd74361bf3
Ethan Turner <ezturner@washington.edu>
2018-03-28 16:36:56 -0700

    Modifying test

--- main_test.go
+++ main_test.go
@@ -8,13 +8,14 @@ import (
 
 func TestHandler(t *testing.T) {
 
-	request := events.APIGatewayProxyRequest{}
+	body := "{ \"secondsToRun\": 2 }"
+	request := events.APIGatewayProxyRequest{Body:body}
 	expectedResponse := events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body: "Congratulations",
+		Body: "Caliper Emitter Worker successfully triggered",
 	}
 
 	response, err := Handler(request)
@@ -22,5 +23,38 @@ func TestHandler(t *testing.T) {
 	assert.Equal(t, response.Headers, expectedResponse.Headers)
 	assert.Contains(t, response.Body, expectedResponse.Body)
 	assert.Equal(t, err, nil)
+
+
+	body = "hello"
+	request = events.APIGatewayProxyRequest{Body:body}
+	expectedResponse = events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+		Body: "Malformed request",
+	}
+
+	response, err = Handler(request)
+
+	assert.Equal(t, response.Headers, expectedResponse.Headers)
+	assert.Contains(t, response.Body, expectedResponse.Body)
+	assert.Equal(t, err, nil)
+}
+
+func TestParseBodyJSON(t *testing.T) {
+	body := "{ \"secondsToRun\": 2 }"
+
+
+	json, err := parseBodyJSON(body)
+
+	expectedJSON := make(map[string]interface{})
+	expectedJSON["secondsToRun"] = float64(2)
+
+	assert.Equal(t, expectedJSON["secondsToRun"], float64(2))
+	assert.Nil(t, err)
+	assert.Equal(t, expectedJSON, json)
+
+
 
 }
